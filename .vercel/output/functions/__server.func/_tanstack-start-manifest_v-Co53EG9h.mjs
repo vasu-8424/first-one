051804f3ededd1016760b04cac0ecb5ba2dc155c
vasu-8424 <vasuunakka@gmail.com>
2026-05-31 15:17:58 +0530

    fix: remove vercel output from git tracking

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-Co53EG9h.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/first/aervo-deliver/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-C-vqofbC.js"] }, "/": { filePath: "D:/first/aervo-deliver/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-DKC5X28O.js"] } }, clientEntry: "/assets/index-C-vqofbC.js" });
-export {
-  tsrStartManifest
-};
